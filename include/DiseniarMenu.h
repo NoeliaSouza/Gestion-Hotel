d7865111f737f2c1e18311b10e2c3f49b30c32dc
Noelia Souza <122423086+NoeliaSouza@users.noreply.github.com>
2023-01-13 00:02:11 -0300

    Add files via upload

---
+++ include/DiseniarMenu.h
@@ -0,0 +1,29 @@
+#ifndef DISENIARMENU_H
+#define DISENIARMENU_H
+#include <locale.h>
+#include <string>
+#include <stdlib.h>
+#include "rlutil.h"
+#include "Fecha.h"
+
+
+class DiseniarMenu
+{
+private:
+    int posX=30, posY=7;
+    void calcularLinea(std::string &cadena,int longitud,char c='-');
+    void calcular_espacios(std::string &palabra,int longitud);
+    void dibujarMenu(std::string *nombresOpciones,std::string &lineaFin,std::string &lineaMed,int &posX, int &posY,int &posCurY,int &cantFilas,bool &color, int &y);
+    void resetearPosicionY(std::string *nombresOpciones,int &y,int &cantFilas, bool &color,int &posX, int &posCurY,int &posY);
+    int posicionarCursor(int &posCurX,int &posCurY,int &y, int &iniciar, int &posX,int &cantFilas);
+
+public:
+    int Menu(std::string* nombresOpciones, int cantFilas,std::string descripcion,int distanciaMenu);
+    int Menu2(std::string* nombresOpciones, int cantFilas,std::string descripcion, int distanciaMenu);
+    void setPosX(int x);
+    void setPosY(int y);
+
+
+};
+
+#endif // DISENIARMENU_H
---
+++ include/Funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include "Fecha.h"
+#include <ctime>
+
+
+void cargarCadena(char *pal, int tam);
+void mostrarMenuPrincipal();
+void mostrarMenuSegundario();
+void mostrarDiskete();
+
+
+
+
+#endif // FUNCIONES_H
